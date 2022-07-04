83de7f0cf1f278fac74c5a5307aed3616a18837c
ghm8614 <ghm8614@gmail.com>
2022-07-04 18:35:05 +0900

    seulmeal main PJT Community Refactoring

---
+++ seulmeal_refactoring/src/test/java/shop/seulmeal/refactoring/SeulmealRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.seulmeal.refactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeulmealRefactoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
