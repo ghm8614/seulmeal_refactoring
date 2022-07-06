84ab47190c32e479db94c92657cf79ea988a89bd
ghm8614 <ghm8614@gmail.com>
2022-07-06 10:25:31 +0900

    jsp에서 restful한 api 설계를 위한 빈등록

---
+++ seulmeal_refactoring/src/main/java/shop/seulmeal/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package shop.seulmeal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class MvcConfig {
+	
+	// method = put, patch, delete 사용을 위한 bean 등록
+	@Bean
+	public HiddenHttpMethodFilter httpMethodFilter() {
+		HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
+		return hiddenHttpMethodFilter;
+	}
+}
